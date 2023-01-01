46537b8fea7d18cfd4eb13d375d3393e654d12b8
Matt Miller <mattmillerf@pm.me>
2023-01-01 15:15:49 -0500

    refactor

--- example_one/src/main.rs
+++ example_one/src/main.rs
@@ -1,13 +1,14 @@
 use std::fs;
+use anyhow::Result;
 
-fn main() {
+fn main() -> Result<()> {
     let mut vec = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
-    let file = fs::read_to_string("array.txt")
-        .expect("Something went wrong reading the file");
+    let file = fs::read_to_string("array.txt")?;
     let mut values = file
         .split(",")
         .filter_map(|x| x.parse().ok())
         .collect::<Vec<i32>>();
     vec.append(&mut values);
-    println!("{:?}", vec)
+    println!("{:?}", vec);
+    Ok(())
 }
