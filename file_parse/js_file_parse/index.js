2603aa642a80cc424295d51f823a884b5ac75384
Matt <mattmillerf@pm.me>
2023-02-20 21:20:40 -0500

    Update index.js

--- file_parse/js_file_parse/index.js
+++ file_parse/js_file_parse/index.js
@@ -1,4 +1,7 @@
 const fs = require("fs")
-const result = fs.readFileSync("../stuff.txt").toString().trim().split("\n")
+const result = fs.readFileSync("../stuff.txt")
+.toString()
+.trim()
+.split("\n")
 console.log(result)
 
