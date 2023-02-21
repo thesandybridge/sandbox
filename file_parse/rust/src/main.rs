b159391c5d5ea2b08dd3a800619054aa902deb2d
Matt Miller <mattmillerf@pm.me>
2023-02-21 12:12:15 -0500

    add extra steps to rust

--- file_parse/rust/src/main.rs
+++ file_parse/rust/src/main.rs
@@ -10,11 +10,23 @@ fn main() {
     let file = fs::read_to_string(path)
         .expect("Something went wrong");
 
-    let lines = file
+    let mut lines = file
         .lines()
-        .collect::<Vec<&str>>();
+        .map(|s| s
+             .chars()
+             .filter_map(|c| c.is_digit(10).then(|| c))
+             .collect::<String>()
+            )
+        .collect::<Vec<String>>();
+    
+    lines.retain(|s| !s.is_empty());
+
+    let filtered_lines = lines
+        .iter()
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>();
 
     let end = start.elapsed();
     println!("Exec Time: {:?}", end);
-    println!("Array Length: {:?}", lines.len());
+    println!("Array Length: {:?}", filtered_lines.len());
 }
