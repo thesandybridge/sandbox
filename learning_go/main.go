1ede2e15e1ee9ea393df374b191416d85859eb77
Matt Miller <mattmillerf@pm.me>
2023-12-26 11:17:38 -0500

    edits

--- learning_go/main.go
+++ learning_go/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+    "bufio"
     "fmt"
+    "os"
+    "strconv"
+    "strings"
     "time"
 )
+
+type Person struct {
+    name string
+    age int
+    job string
+}
 
 func formatTime(t time.Time) string {
     return t.Format("01-02-2006 15:04")
@@ -11,13 +21,43 @@ func formatTime(t time.Time) string {
 
 func handleInput(message string) string {
     fmt.Print(message)
-    var input string
-    fmt.Scanln(&input)
-    return input
+    reader := bufio.NewReader(os.Stdin)
+    input, err := reader.ReadString('\n')
+    if err != nil {
+        fmt.Println("Error reading input:", err)
+        return ""
+    }
+    return strings.TrimSpace(input)
+}
+
+func createPerson() Person {
+    name := handleInput("What is your name? ")
+    ageStr := handleInput("What is your age? ")
+    age, err := strconv.ParseInt(ageStr, 10, 64)
+    if err != nil {
+        fmt.Println("Invalid input for age. Using default age 0.")
+        age = 0
+    }
+    job := handleInput("What is your job? ")
+    return Person{name, int(age), job}
+}
+
+func writePersonToFile() {
+    person := createPerson()
+    file, err := os.Create("person.txt")
+    if err != nil {
+        fmt.Println("Error creating file:", err)
+        return
+    }
+    defer file.Close()
+    _, err = file.WriteString(fmt.Sprintf("%s,%d,%s", person.name, person.age, person.job))
+
+    if err != nil {
+        fmt.Println("Error writing to file:", err)
+        return
+    }
 }
 
 func main() {
-    input := handleInput("What is your name? ")
-    message := fmt.Sprintf("Hello, %s! The time is %s", input, formatTime(time.Now()))
-    fmt.Println(message)
+    writePersonToFile()
 }
