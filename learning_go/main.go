26af9b7fdefaf8b96aa7358a62ba43a59abe9901
Matt Miller <mattmillerf@pm.me>
2023-12-26 11:23:45 -0500

    format

--- learning_go/main.go
+++ learning_go/main.go
@@ -1,58 +1,58 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "strconv"
-    "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type Person struct {
-    name string
-    age int
-    job string
+	name string
+	age  int
+	job  string
 }
 
 func handleInput(message string) string {
-    fmt.Print(message)
-    reader := bufio.NewReader(os.Stdin)
-    input, err := reader.ReadString('\n')
-    if err != nil {
-        fmt.Println("Error reading input:", err)
-        return ""
-    }
-    return strings.TrimSpace(input)
+	fmt.Print(message)
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return ""
+	}
+	return strings.TrimSpace(input)
 }
 
 func createPerson() Person {
-    name := handleInput("What is your name? ")
-    ageStr := handleInput("What is your age? ")
-    age, err := strconv.ParseInt(ageStr, 10, 64)
-    if err != nil {
-        fmt.Println("Invalid input for age. Using default age 0.")
-        age = 0
-    }
-    job := handleInput("What is your job? ")
-    return Person{name, int(age), job}
+	name := handleInput("What is your name? ")
+	ageStr := handleInput("What is your age? ")
+	age, err := strconv.ParseInt(ageStr, 10, 64)
+	if err != nil {
+		fmt.Println("Invalid input for age. Using default age 0.")
+		age = 0
+	}
+	job := handleInput("What is your job? ")
+	return Person{name, int(age), job}
 }
 
 func writePersonToFile() {
-    person := createPerson()
-    file, err := os.Create("person.txt")
-    if err != nil {
-        fmt.Println("Error creating file:", err)
-        return
-    }
-    defer file.Close()
-    _, err = file.WriteString(fmt.Sprintf("%s,%d,%s", person.name, person.age, person.job))
+	person := createPerson()
+	file, err := os.Create("person.txt")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+	_, err = file.WriteString(fmt.Sprintf("%s,%d,%s", person.name, person.age, person.job))
 
-    if err != nil {
-        fmt.Println("Error writing to file:", err)
-        return
-    }
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
 }
 
 func main() {
-    writePersonToFile()
+	writePersonToFile()
 }
