7c59c3ae5de4c7d0a05b1b4d065d3bfb2b51201a
Matt Miller <mattmillerf@pm.me>
2023-12-26 10:51:48 -0500

    adds simple input command

--- learning_go/main.go
+++ learning_go/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    fmt.Print("Enter your name: ")
+    var input string
+    fmt.Scanln(&input)
+    fmt.Println("Hello, " + input + "!")
 }
