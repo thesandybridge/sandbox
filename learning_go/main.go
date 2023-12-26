0b9fab4f57cce804f2569ea3af2190f52178de63
Matt Miller <mattmillerf@pm.me>
2023-12-26 11:00:33 -0500

    refactor

--- learning_go/main.go
+++ learning_go/main.go
@@ -2,11 +2,22 @@ package main
 
 import (
     "fmt"
+    "time"
 )
 
-func main() {
-    fmt.Print("Enter your name: ")
+func formatTime(t time.Time) string {
+    return t.Format("01-02-2006 15:04")
+}
+
+func handleInput(message string) string {
+    fmt.Print(message)
     var input string
     fmt.Scanln(&input)
-    fmt.Println("Hello, " + input + "!")
+    return input
+}
+
+func main() {
+    input := handleInput("What is your name? ")
+    message := fmt.Sprintf("Hello, %s! The time is %s", input, formatTime(time.Now()))
+    fmt.Println(message)
 }
